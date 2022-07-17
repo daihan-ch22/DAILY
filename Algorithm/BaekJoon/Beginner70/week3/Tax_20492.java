d01d0c6c96a449116a8ea149dc11fbd23b19620d
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-07-17 15:07:29 +0900

    algo practice 17July

---
+++ Algorithm/BaekJoon/Beginner70/week3/Tax_20492.java
@@ -0,0 +1,19 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Tax_20492 {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader bf =
+                new BufferedReader(new InputStreamReader(System.in));
+        int prize = Integer.parseInt(bf.readLine());
+        bf.close();
+
+        int case1 = prize - (int)(prize*0.22);
+        int case2 = (int) (prize - ((prize - (int)(prize * 0.8)) * 0.22));
+
+        System.out.printf("%d %d", case1, case2);
+    }
+}
